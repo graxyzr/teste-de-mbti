3d797467adc8e77fd4dc7300bfb01041d06b57b3
Greice Pereira <118115939+graxyzr@users.noreply.github.com>
2023-07-04 20:49:55 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,381 @@
+function verificaAnwers() {
+
+    if (document.forms["question1"]["question1"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question2"]["question2"].value == "op1") {
+        pontodiplomata += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question3"]["question3"].value == "op2") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question4"]["question4"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question5"]["question5"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question6"]["question6"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question7"]["question7"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question8"]["question8"].value == "op1") {
+        pontodiplomata += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question9"]["question9"].value == "op1") {
+        pontodiplomata += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question10"]["question10"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question11"]["question11"].value == "op1") {
+        pontoanalista += 5;
+        pontodiplomata += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question12"]["question12"].value == "op1") {
+        pontoanalista += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question13"]["question13"].value == "op1") {
+        pontoanalista += 5;
+        pontoexplorador += 5;
+        pontodiplomata += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question14"]["question14"].value == "op1") {
+        pontosentinela += 5;
+        pontoanalista += 5;
+    }
+    else {
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question15"]["question15"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontosentinela += 5;
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question16"]["question16"].value == "op1") {
+        pontoanalista += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontodiplomata += 5;
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question17"]["question17"].value == "op1") {
+        pontoanalista += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question18"]["question18"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question19"]["question19"].value == "op1") {
+        pontodiplomata += 5;
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question20"]["question20"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontoexplorador += 5;
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question21"]["question21"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question22"]["question22"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question23"]["question23"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question24"]["question24"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question25"]["question25"].value == "op1") {
+        pontoexplorador += 5;
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question26"]["question26"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question27"]["question27"].value == "op1") {
+        pontoanalista += 5;
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question28"]["question28"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question29"]["question29"].value == "op1") {
+        pontoexplorador += 5;
+        pontoanalista += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question30"]["question30"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question31"]["question31"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question32"]["question32"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question33"]["question33"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question34"]["question34"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question35"]["question35"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question36"]["question36"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question37"]["question37"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question38"]["question38"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question39"]["question39"].value == "op1") {
+        pontoexplorador += 5;
+        pontodiplomata += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question40"]["question40"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question41"]["question41"].value == "op1") {
+        pontoexplorador += 5;
+        pontoanalista += 5;
+    }
+    else {
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question42"]["question42"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontodiplomata += 5;
+    }
+
+    if (document.forms["question43"]["question43"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question44"]["question44"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoexplorador += 5;
+    }
+
+    if (document.forms["question45"]["question45"].value == "op1") {
+        pontodiplomata += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question46"]["question46"].value == "op1") {
+        pontosentinela += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    if (document.forms["question47"]["question47"].value == "op1") {
+        pontoanalista += 5;
+    }
+    else {
+        pontodiplomata += 5;
+        pontosentinela += 5;
+    }
+
+    if (document.forms["question48"]["question48"].value == "op1") {
+        pontoexplorador += 5;
+    }
+    else {
+        pontoanalista += 5;
+    }
+
+    resp = Math.max(pontodiplomata, pontoanalista, pontoexplorador, pontosentinela);
+    if (resp == pontodiplomata) {
+        document.getElementById("anw1").innerHTML = "Com " + resp + "% sua personalidade é o Diplomata.";
+    }
+    if (resp == pontoanalista) {
+        document.getElementById("anw2").innerHTML = "Com " + resp + "% sua personalidade é o Analista.";
+    }
+    if (resp == pontoexplorador) {
+        document.getElementById("anw3").innerHTML = "Com " + resp + "% sua personalidade é o Explorador.";
+    }
+    if (resp == pontosentinela) {
+        document.getElementById("anw4").innerHTML = "Com " + resp + "% sua personalidade é o Sentinela.";
+    }
+
+}
+
+var pontoexplorador = 0, pontosentinela = 0, pontoanalista = 0, pontodiplomata = 0;
+
+var finalizar = document.getElementById("finalizar");
+finalizar.addEventListener("click", verificaAnwers);
